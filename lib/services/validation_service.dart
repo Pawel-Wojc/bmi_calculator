a26f3daf051b8760a6de362e6bc3b5796fc79416
Paweł Wójcik <62841636+Pawel-Wojc@users.noreply.github.com>
2024-12-11 11:59:21 +0100

    update validation service
    update validation service to handle different units due to validation

--- lib/services/validation_service.dart
+++ lib/services/validation_service.dart
@@ -1,14 +1,22 @@
+import 'package:bmi_app/services/units_calculator_service.dart';
+
+import '../data/bmi_data.dart';
+
 class ValidationService {
   static final RegExp emailRegex =
       RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
 
-  static String? validateWeight(String? value) {
+  static String? validateWeight(String? value, UnitsSystem units) {
     if (value == null || value.isEmpty) {
       return '';
     }
     value = value.replaceAll(',', '.');
     double? parsedValue;
     int index = value.indexOf('.');
+
+    value = UnitsCalculatorService.recalculateWeight(
+        units, UnitsSystem.metric, value);
+    print('validation weight: $value');
     if (index != -1) {
       if (value.substring(index + 1).length > 2) {
         return '';
@@ -25,18 +33,23 @@ class ValidationService {
     return null;
   }
 
-  static String? validateHeight(String? value) {
+  static String? validateHeight(String? value, UnitsSystem units) {
     if (value == null || value.isEmpty) {
       return '';
     }
     value = value.replaceAll(',', '.');
     double? parsedValue;
+
+    value = UnitsCalculatorService.recalculateHeight(
+        units, UnitsSystem.metric, value);
+    print('validation height: $value');
     try {
       parsedValue = double.parse(value);
     } catch (e) {
       return '';
     }
-    if (parsedValue > 2.5 || parsedValue < 0.01) {
+    if ((parsedValue > 2.5) || parsedValue < 0.01) {
+      print(parsedValue);
       return '';
     }
     return null;
